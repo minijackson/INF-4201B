382be1cec8c9dff76613c2e89af8bc675767bca1
Minijackson <minijackson@riseup.net>
2017-01-21 17:51:11 +0100

    Remove client, and move server up

--- client/test/client_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ClientTest do
-  use ExUnit.Case
-  doctest Client
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
--- config/config.exs
+++ config/config.exs
@@ -10,15 +10,15 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :client, key: :value
+#     config :server, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:client, :key)
+#     Application.get_env(:server, :key)
 #
 # Or configure a 3rd-party app:
 #
-#     config :logger, level: :info
+config :logger, level: :warn
 #
 
 # It is also possible to import configuration files, relative to this
@@ -28,3 +28,7 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+
+config :server,
+  nodes: [{'hostname', 4200}, {'hostname', 4201}, {'hostname', 4202}, {'hostname', 4203}]
